2fea4b8505bf2d09c383801d01f10d3740047b73
ka16 <69737968+ka16@users.noreply.github.com>
2020-12-15 18:12:28 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function () {
+  // 親メニュー処理
+  $('span').click(function() {
+    // メニュー表示/非表示
+    $(this).next('ul').slideToggle('fast');
+  });
+
+  // 子メニュー処理
+  $('li').click(function(e) {
+    // メニュー表示/非表示
+    $(this).children('ul').slideToggle('fast');
+    e.stopPropagation();
+  });
+});
